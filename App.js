812079ae5bb86e3054c7038ec5f140421b64de33
aalexblade <79399963+aalexblade@users.noreply.github.com>
2023-05-23 22:07:59 +0300

    dismiss

--- App.js
+++ App.js
@@ -1,50 +1,86 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, TextInput, Button } from 'react-native';
+import {
+   StyleSheet,
+   Text,
+   View,
+   ImageBackground,
+   TextInput,
+   TouchableOpacity,
+   Platform,
+   KeyboardAvoidingView,
+   Keyboard,
+} from 'react-native';
 
 export default function App() {
+  
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false)
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false)
+    Keyboard.dismiss()
+  }
+
   return (
     <View style={styles.container}>
       <ImageBackground
-        style={styles.image}
+        style={styles.background}
         source={require("./assets/img/PhotoBG.jpg")}
       >
+        <KeyboardAvoidingView
+         behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
         <View
-          style={styles.form}
+          style={{ ...styles.form, marginBottom : isShowKeyboard ? -100 : 0}}
+          
         >
           <View>
-            <Text style={styles.text}>Ругистрация</Text>
-            <Text style={styles.inputTitle}></Text>
+            <Text style={styles.text}>Реєстрація</Text>
+            
         <TextInput
           style={styles.input}
           textAlign={"left"}
+          placeholder={"Логін"}
+          onFocus={()=>setIsShowKeyboard(true)}
           // onChange={onChangeText}
           // value={text}
-          placeholder={"Логин"}
             />
-            <View style={{marginTop:16}}>
+            <View>
            <Text style={styles.inputTitle}></Text>
         <TextInput
           style={styles.input}
           textAlign={"left"}
+          placeholder={"Адреса електронної пошти"}
+          onFocus={()=>setIsShowKeyboard(true)}
           // onChange={onChangeText}
           // value={"Адрес электронной почты"}
-          placeholder={"Адрес электронной почты"}
               />
             </View>
-            <View style={{marginTop: 16}}>
+            <View>
            <Text style={styles.inputTitle}></Text>
         <TextInput
           style={styles.input}
           textAlign={"left"}
+          placeholder={"Пароль"}
+          onFocus={()=>setIsShowKeyboard(true)}
           // onChange={onChangeText}
           // value={"Пароль"}
-          placeholder={"Пароль"}
           secureTextEntry={true}
               />
               </View>
           </View>
-          <Button title="Зарегистрироваться"/>
+          <TouchableOpacity 
+            activeOpacity={0.8} 
+            style={styles.btn}
+            onPress={keyboardHide}
+          >
+            <Text style={styles.btnText}>Зареєструватися</Text>
+          </TouchableOpacity>
+           <TouchableOpacity activeOpacity={0.8}>
+            <Text style={styles.logInText}>Вже є аккаунт? Увійти</Text>
+          </TouchableOpacity>
           </View>
+        </KeyboardAvoidingView>
       </ImageBackground>
         <StatusBar style="auto" />
     </View>
@@ -54,24 +90,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "ffffff",
+    
    
   },
-    image: {
+    background: {
     flex: 1,
     resizeMode:"cover",
-    justifyContent: "center", 
+    justifyContent:"flex-end",
+    // justifyContent: "center", 
     // alignItems:"center",
   },
   text: {
     color: "#212121",
+    textAlign: "center",
     fontSize: 30,
-    marginLeft: 96,
-    marginRight: 95,
-    // marginEnd: 33,
+    marginBottom: 33,
   },
   form: { 
-    marginHorizontal: 16,
+    backgroundColor: "#ffffff",
+    paddingHorizontal: 16,
+    paddingTop:92,
+    paddingBottom: 78,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    
+    
   },
   input: {
     borderWidth: 1,
@@ -86,7 +130,26 @@ const styles = StyleSheet.create({
     
   },
   inputTitle:{
-  color:"#bdbdbd",
-  }
+    color:"#bdbdbd",
+  },
+  btn: {
+    backgroundColor: "#ff6c00",
+    height:51,
+    borderRadius: 100,
+    marginTop: 43,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnText: {
+    color: "#ffffff",
+    fontSize: 16,
+  },
+  logInText: {
+    color: "#1b4371",
+    textAlign: "center",
+    fontSize: 16,
+    marginTop: 16,
+    // lineHeight: 19,
+  },
 
 });
